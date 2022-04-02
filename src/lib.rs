2df42ae4a3339348803cdd564afacea018a1aa44
Fuwn <contact@fuwn.net>
2022-04-02 00:52:49 +0000

    docs(cargo): bump version to 0.1.8

--- src/lib.rs
+++ src/lib.rs
@@ -32,11 +32,15 @@
 //! # Cargo.toml
 //!
 //! [dependencies]
-//! windmark = "0.1.7"
+//! windmark = "0.1.8"
 //! tokio = { version = "0.2.4", features = ["full"] }
 //!
 //! # If you would like to use the built-in logger (recommended)
-//! # windmark = { version = "0.1.7", features = ["logger"] }
+//! # windmark = { version = "0.1.8", features = ["logger"] }
+//!
+//! # If you would like to use the built-in MIME dedection when `Success`-ing a file
+//! # (recommended)
+//! # windmark = { version = "0.1.8", features = ["auto-deduce-mime"] }
 //! ```
 //!
 //! ### Implement a Windmark server
