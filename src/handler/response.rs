9e197aef0e34583e5a5de6abe4999f5cbb6591c2
Fuwn <contact@fuwn.net>
2023-04-03 03:13:00 +0000

    refactor(handler): move traits

---
+++ src/handler/response.rs
@@ -0,0 +1,25 @@
+// This file is part of Windmark <https://github.com/gemrest/windmark>.
+// Copyright (C) 2022-2022 Fuwn <contact@fuwn.me>
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, version 3.
+//
+// This program is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// Copyright (C) 2022-2022 Fuwn <contact@fuwn.me>
+// SPDX-License-Identifier: GPL-3.0-only
+
+#![allow(clippy::module_name_repetitions)]
+
+mod error;
+mod route;
+
+pub use error::ErrorResponse;
+pub use route::RouteResponse;
