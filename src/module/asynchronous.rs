c92ccc067d9c1164121db18180a5c774ea298668
Fuwn <contact@fuwn.me>
2023-04-04 19:52:53 -0700

    fix(module): bring back

---
+++ src/module/asynchronous.rs
@@ -0,0 +1,31 @@
+// This file is part of Windmark <https://github.com/gemrest/windmark>.
+// Copyright (C) 2022-2022 Fuwn <contact@fuwn.me>
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, version 3.
+//
+// This program is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// Copyright (C) 2022-2022 Fuwn <contact@fuwn.me>
+// SPDX-License-Identifier: GPL-3.0-only
+
+use crate::{context::HookContext, Router};
+
+#[async_trait::async_trait]
+pub trait AsyncModule: Send + Sync {
+  /// Called right after the module is attached.
+  async fn on_attach(&mut self, _: &mut Router) {}
+
+  /// Called before a route is mounted.
+  async fn on_pre_route(&mut self, _: HookContext<'_>) {}
+
+  /// Called after a route is mounted.
+  async fn on_post_route(&mut self, _: HookContext<'_>) {}
+}
