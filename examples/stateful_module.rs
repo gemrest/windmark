732bf8ada3bd2f0d76c6c3de9367176c4d17715d
Fuwn <contact@fuwn.net>
2023-04-15 01:53:17 +0000

    chore(examples): add sync module example

---
+++ examples/stateful_module.rs
@@ -0,0 +1,67 @@
+// This file is part of Windmark <https://github.com/gemrest/windmark>.
+// Copyright (C) 2022-2022 Fuwn <contact@fuwn.me>
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, version 3.
+//
+// This program is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// Copyright (C) 2022-2022 Fuwn <contact@fuwn.me>
+// SPDX-License-Identifier: GPL-3.0-only
+
+//! `cargo run --example stateful_module`
+
+use windmark::{context::HookContext, Router};
+
+#[derive(Default)]
+struct Clicker {
+  clicks: usize,
+}
+
+impl windmark::Module for Clicker {
+  fn on_attach(&mut self, _router: &mut Router) {
+    println!("module 'clicker' has been attached!");
+  }
+
+  fn on_pre_route(&mut self, context: HookContext) {
+    self.clicks += 1;
+
+    println!(
+      "module 'clicker' has been called before the route '{}' with {} clicks!",
+      context.url.path(),
+      self.clicks,
+    );
+  }
+
+  fn on_post_route(&mut self, context: HookContext) {
+    println!(
+      "module 'clicker' clicker has been called after the route '{}' with {} \
+       clicks!",
+      context.url.path(),
+      self.clicks,
+    );
+  }
+}
+
+#[windmark::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+  let mut router = Router::new();
+
+  router.set_private_key_file("windmark_private.pem");
+  router.set_certificate_file("windmark_public.pem");
+  #[cfg(feature = "logger")]
+  {
+    router.enable_default_logger(true);
+  }
+  router.attach(Clicker::default());
+  router.mount("/", windmark::success!("Hello!"));
+
+  router.run().await
+}
