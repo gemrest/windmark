c360d1d773bbfa9c9cfec97a202cc2b29b141010
Fuwn <contact@fuwn.me>
2023-04-15 02:00:45 +0000

    refactor(hooks): implement call for hooks

--- src/handler/hooks/pre_route.rs
+++ src/handler/hooks/pre_route.rs
@@ -19,6 +19,12 @@
 use crate::context::HookContext;
 
 #[allow(clippy::module_name_repetitions)]
-pub trait PreRouteHook: FnMut(HookContext) + Send + Sync {}
+pub trait PreRouteHook: Send + Sync {
+  fn call(&mut self, context: HookContext);
+}
 
-impl<T> PreRouteHook for T where T: FnMut(HookContext) + Send + Sync {}
+impl<T> PreRouteHook for T
+where T: FnMut(HookContext) + Send + Sync
+{
+  fn call(&mut self, context: HookContext) { (*self)(context) }
+}
