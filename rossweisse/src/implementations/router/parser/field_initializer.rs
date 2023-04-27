a6cdabeb72cb4de1adfca6be708859768ee044cf
Fuwn <contact@fuwn.net>
2023-04-27 05:52:31 +0000

    feat(rossweisse): initial implementation of struct router framework

---
+++ rossweisse/src/implementations/router/parser/field_initializer.rs
@@ -0,0 +1,39 @@
+// This file is part of Windmark <https://github.com/gemrest/windmark>.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, version 3.
+//
+// This program is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// Copyright (C) 2022-2023 Fuwn <contact@fuwn.me>
+// SPDX-License-Identifier: GPL-3.0-only
+
+use syn::parse;
+
+pub struct FieldInitializer {
+  pub ident: syn::Ident,
+  #[allow(unused)]
+  eq_token:  syn::Token![=],
+  pub expr:  syn::Expr,
+}
+
+impl parse::Parse for FieldInitializer {
+  fn parse(input: parse::ParseStream<'_>) -> syn::Result<Self> {
+    let ident = input.parse()?;
+    let eq_token = input.parse()?;
+    let expr = input.parse()?;
+
+    Ok(Self {
+      ident,
+      eq_token,
+      expr,
+    })
+  }
+}
