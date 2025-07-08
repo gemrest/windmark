4af8167c72a75b1af7c4b4dcb54bd4780d9465b0
Fuwn <contact@fuwn.me>
2025-07-07 22:23:11 -0700

    docs(router_option): Expand documentation

--- src/router_option.rs
+++ src/router_option.rs
@@ -1,10 +1,10 @@
 /// Options that can be set for the `Router`
 #[derive(Debug, PartialEq, Eq, Hash, Clone, Copy)]
 pub enum RouterOption {
-  /// Trim trailing slashes from the URL path if it is present and a route
-  /// match exists
+  /// If enabled, removes a trailing slash from the request URL path if a route
+  /// exists for the path without the slash (e.g., `/foo/` becomes `/foo`).
   RemoveExtraTrailingSlash,
-  /// Add a trailing slash to the URL path if it is missing and a route
-  /// match exists
+  /// If enabled, adds a trailing slash to the request URL path if a route
+  /// exists for the path with the slash (e.g., `/foo` becomes `/foo/`).
   AddMissingTrailingSlash,
 }
