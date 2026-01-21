43fb2c613448589e0bac61996f38653cbefebd45
Fuwn <contact@fuwn.me>
2026-01-21 09:03:05 +0000

    fix(rossweisse): Fix all clippy pedantic and nursery lint errors

--- rossweisse/src/implementations/route.rs
+++ rossweisse/src/implementations/route.rs
@@ -17,6 +17,6 @@
 
 use proc_macro::TokenStream;
 
-pub fn route(_arguments: TokenStream, item: syn::ItemFn) -> TokenStream {
+pub fn route(_arguments: TokenStream, item: &syn::ItemFn) -> TokenStream {
   quote::quote! { #item }.into()
 }
