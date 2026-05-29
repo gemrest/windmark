8ec90dc75b368c637c135d7eaf86cddf5e5ffe6e
Fuwn <contact@fuwn.net>
2026-05-29 12:56:38 +0000

    refactor(module): Collapse the sync and async module traits into a single file

--- src/module.rs
+++ src/module.rs
@@ -1,6 +1,24 @@
-mod asynchronous;
-mod sync;
+use crate::context::HookContext;
 
-#[allow(clippy::module_name_repetitions)]
-pub use asynchronous::AsyncModule;
-pub use sync::Module;
+pub trait Module {
+  /// Called right after the module is attached.
+  fn on_attach(&mut self, _: &mut crate::router::Router) {}
+
+  /// Called before a route is mounted.
+  fn on_pre_route(&mut self, _: &HookContext) {}
+
+  /// Called after a route is mounted.
+  fn on_post_route(&mut self, _: &HookContext) {}
+}
+
+#[async_trait::async_trait]
+pub trait AsyncModule: Send + Sync {
+  /// Called right after the module is attached.
+  async fn on_attach(&mut self, _: &mut crate::router::Router) {}
+
+  /// Called before a route is mounted.
+  async fn on_pre_route(&mut self, _: &HookContext) {}
+
+  /// Called after a route is mounted.
+  async fn on_post_route(&mut self, _: &HookContext) {}
+}
